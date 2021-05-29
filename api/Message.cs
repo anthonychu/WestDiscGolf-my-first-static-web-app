5377aaace9394d7d98d7ac4f77728d750dfdc80f
Adam Storr <astorr@gmail.com>
2021-05-29 22:14:05 +0100

    new c# api

---
+++ api/Message.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class Message
+    {
+        [FunctionName("Message")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var responseMessage = new { text = "Hello from the .NET API!" };
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
